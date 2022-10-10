6b7120934235716fcdbea5023632fd5427eecad2
binhe924@163.com <binhe924@163.com>
2022-10-10 22:37:36 +0800

    修复（入口文件）：设备未预设主题时设置默认主题

--- App.vue
+++ App.vue
@@ -6,6 +6,15 @@
 			}
 		},
 		async onLaunch() {
+			// 设置预设主题
+			if( !this.vuex_themeColor.colorCode ){
+				this.$u.vuex('vuex_themeColor',{
+					color: "#0081ff",
+					colorCharacter: "bg-gradual-blue",
+					colorCode: "linear-gradient(45deg, #0081ff, #1cbbb4)",
+					colorTitle: "魔幻蓝"
+				});
+			}
  			// #ifdef APP-PLUS
 			// 获取app版本信息 用于版本更新
 			let res = await this.$iipKit.checkVersion(this);
