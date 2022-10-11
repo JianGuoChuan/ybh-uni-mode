53c3b0ca21ecfc0a5739f7bf7d631b811571be7a
binhe924@163.com <binhe924@163.com>
2022-10-12 00:52:01 +0800

    优化（路由表）：路由函数迁移

--- common/router/router.js
+++ common/router/router.js
@@ -27,6 +27,37 @@ let router = {
 		uni.navigateBack({
 			delta
 		})
-	}
+	},
+	// 延迟跳转系列
+	delayBack(delay = 1000, delta = 1) {
+	    setTimeout(() => {
+	        uni.navigateBack({
+				delta: delta
+	        });
+	    }, delay);
+	},
+	delayToRouter(path, delay = 1000,data){
+		setTimeout(() => {
+		    let toUrl = path + '?' + qs.stringify(data);
+		    uni.navigateTo({
+		    	url : toUrl
+		    })
+		}, delay);
+	},
+	delayReLaunchRouter(path, delay = 1000,data){
+		setTimeout(() => {
+		    let toUrl = path + '?' + qs.stringify(data);
+		    uni.reLaunch({
+		    	url : toUrl
+		    })
+		}, delay);
+	},
+	delaySwitchTabRouter( path, delay = 1000){
+		setTimeout(() => {
+		    uni.switchTab({
+		    	url : path
+		    })
+		}, delay);
+	},
 }
 export default router;
