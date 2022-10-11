6624ffa8586ff71166d5b30c6471cb3429e35eaf
binhe924@163.com <binhe924@163.com>
2022-10-12 00:54:11 +0800

    优化（用户信息卡）：设置默认样式，支持完全自定义

--- components/ybh/ybh-userCard/ybh-userCard.vue
+++ components/ybh/ybh-userCard/ybh-userCard.vue
@@ -1,19 +1,23 @@
 <template>
 	<view>
-		<view :class="[vuex_themeColor.colorCharacter || 'bg-gradual-blue']" class="card_box">
+		<view :class="[vuex_themeColor.colorCharacter || 'bg-gradual-blue', 'card_box']" :style="{ 'paddingTop': `${paddingTop}px`, 'height': `${height}px` }">
+			<!-- 顶部操作栏 -->
 			<view class="padding-lr-lg flex space-between padding-bottom-sm">
 				<view>
-					<u-icon name="setting-fill" size="25px" color="#fff" class="margin-right-sm" />
-					<u-icon name="scan" size="22px" color="#fff" />
+					<u-icon name="setting-fill" size="25px" color="#fff" class="margin-right-sm" @tap='setting'/>
+					<u-icon name="scan" size="22px" color="#fff" @tap='scan'/>
 				</view>
-				<view class="margin-top-sm margin-right-sm relative">
+				<view class="margin-top-sm margin-right-sm relative" @tap='notice'>
 					<u-icon name="chat" size="16px" color="#fff" class="margin-right-xs"></u-icon>
 					<text>消息</text>
 					<u-badge type="error" :is-dot="true" style="right: -3px;top: 0px;"></u-badge>
 				</view>
 			</view>
-			<view style="height: 120px;" class="bg-white padding-lr-md margin-lr-lg border-radius-md shadow-md-bottom">
-				{{vuex_themeColor.colorCharacte}}
+			<!-- 内容区域 -->
+			<view :style="{ 'height': `${contentHeight}px` }" class="bg-white padding-lr-md margin-lr-lg border-radius-md shadow-md-bottom">
+				<template>
+					<slot name='userCardContent'></slot>
+				</template>
 			</view>
 		</view>
 		<view class="padding-tb-md margin-tb-md"></view>
@@ -21,13 +25,35 @@
 </template>
 <script>
 	export default {
+		props:{
+			paddingTop: {
+				type: String,
+				default: '15'
+			},
+			height: {
+				type: String,
+				default: '140'
+			},
+			contentHeight: {
+				type: String,
+				default: '120'
+			}
+		},
 		data(){
 			return{
-				userInfo : {}
+				userInfo : {},
 			}
 		},
 		methods:{
-			
+			setting(){
+				this.$emit( 'set', {key : 0} )
+			},
+			scan(){
+				this.$emit( 'scan', {key : 1} )
+			},
+			notice(){
+				this.$emit( 'notice', {key : 2} )
+			}
 		},
 		created() {
 			this.userInfo = this.vuex_userInfo || {}
@@ -37,8 +63,6 @@
 
 <style lang='scss' scoped>
 	.card_box{
-		height: 140px;
-		padding-top: 15px;
 		border-bottom-left-radius: 10px;
 		border-bottom-right-radius: 10px;
 	}
