97c3583061c769eb02c5e5c966a35092c8675787
binhe924@163.com <binhe924@163.com>
2022-10-14 18:27:08 +0800

    优化（路由）：延迟路由添加回调

--- common/router/router.js
+++ common/router/router.js
@@ -26,37 +26,41 @@ let router = {
 	back(delta){
 		uni.navigateBack({
 			delta
-		})
+		});
 	},
 	// 延迟跳转系列
-	delayBack(delay = 1000, delta = 1) {
+	delayBack(delay = 1000, delta = 1, callback) {
 	    setTimeout(() => {
 	        uni.navigateBack({
 				delta: delta
 	        });
+			callback();
 	    }, delay);
 	},
-	delayToRouter(path, delay = 1000,data){
+	delayToRouter(path, delay = 1000, data, callback){
 		setTimeout(() => {
 		    let toUrl = path + '?' + qs.stringify(data);
 		    uni.navigateTo({
 		    	url : toUrl
-		    })
+		    });
+			callback();
 		}, delay);
 	},
-	delayReLaunchRouter(path, delay = 1000,data){
+	delayReLaunchRouter(path, delay = 1000, data, callback){
 		setTimeout(() => {
 		    let toUrl = path + '?' + qs.stringify(data);
 		    uni.reLaunch({
 		    	url : toUrl
-		    })
+		    });
+			callback();
 		}, delay);
 	},
-	delaySwitchTabRouter( path, delay = 1000){
+	delaySwitchTabRouter( path, delay = 1000, callback){
 		setTimeout(() => {
 		    uni.switchTab({
 		    	url : path
-		    })
+		    });
+			callback();
 		}, delay);
 	},
 }
