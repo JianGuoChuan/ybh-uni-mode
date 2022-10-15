8a51e7ac79cf302bc162e62b1479d33945c8113d
binhe924@163.com <binhe924@163.com>
2022-10-15 21:23:16 +0800

    优化（柱形图）：数据更新演示

--- pages/tools/charts/column.vue
+++ pages/tools/charts/column.vue
@@ -4,6 +4,9 @@
 		<view class="canvasBox margin-top-lgx">
 			<canvas canvas-id="myid" id="myid" class="charts" @tap="tap"/>
 		</view>
+		<view class="padding-md">
+			<ybh-button type="pri" @click='updateDate'>更新数据</ybh-button>
+		</view>
 	</view>
 </template>
 <script>
@@ -13,10 +16,11 @@
 		data(){
 			return {
 				cWidth: 750,
-				cHeight: 280
+				cHeight: 280,
 			}
 		},
 		methods: {
+			// 数据
 			getServerData() {
 				//模拟从服务器获取数据时的延时
 				setTimeout(() => {
@@ -24,8 +28,10 @@
 						categories: ["2016","2017","2018","2019","2020","2021"],
 						series: [
 							{
-								name: "目标值",
-								data: [35,36,31,33,13,34]
+								name: "目标值", // 数据名称
+								legendText: '期望值', // 自定义图例显示文字
+								data: [35,36,31,33,13,34] ,// 数据值
+								show: true, // 是否显示
 							},{
 								name: "完成量",
 								data: [18,27,21,24,6,28]
@@ -35,36 +41,63 @@
 					this.drawCharts('myid', res);
 				}, 500);
 			},
+			// 图表参数配置
 			drawCharts(id,data){
 				const ctx = uni.createCanvasContext(id, this);
 				uChartsInstance[id] = new uCharts({
-					type: "column",
+					type: 'column',
+					padding: [25,15,0,5],
 					context: ctx,
 					width: this.cWidth,
 					height: this.cHeight,
 					categories: data.categories,
 					series: data.series,
-					animation: true,
+					animation: true, // 是否开启动画
+					// 图例配置
+					legend: {
+						show: true, // 是否显示图例标识
+					},
+					// X轴配置
 					xAxis: {
 						disableGrid: true
 					},
+					// Y轴配置
 					yAxis: {
 						data: [ { min: 0 } ]
 					},
 					extra: {
 						column: {
-							type: "group"
+							type: "group",
+							width: 30,
+							activeBgColor: "#000000",
+							activeBgOpacity: 0.08
 						}
-					}
+					},
 				});
 			},
+			// 图表点击
 			tap(e){
 				uChartsInstance[e.target.id].touchLegend(e);
 				uChartsInstance[e.target.id].showToolTip(e);
+			},
+			updateDate(){
+				let res = {
+					categories: ["2016","2017","2018","2019","2020","2021"],
+					series: [
+						{
+							name: "目标值",
+							data: [55,44,33,22,11,0]
+						},{
+							name: "完成量",
+							data: [0,5,10,15,20,25]
+						}
+					]
+				};
+				this.drawCharts('myid', res);
 			}
 		},
+		// 图表初始化
 		onReady() {
-			//这里的 750 对应 css .charts 的 width
 			this.cWidth = uni.upx2px(750);
 			this.$nextTick(() => {
 				let info = uni.createSelectorQuery().in(this).select('.canvasBox');
